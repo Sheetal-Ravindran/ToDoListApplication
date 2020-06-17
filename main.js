f0a4c8234c3d13d2ba31b5e45eaaa34b37c7aa40
Rahul <rash235r@gmail.com>
2020-06-17 13:25:40 +0200

    use let and const

--- main.js
+++ main.js
@@ -1,45 +1,45 @@
 // Create a "close" button and append it to each list item
-var myNodeList = document.getElementsByTagName("li");
-var i;
+const myNodeList = document.getElementsByTagName("li");
+let i;
 for (i = 0; i < myNodeList.length; i++) {
-  var span = document.createElement("button");
-  var txt = document.createTextNode("\u00D7");
+  const span = document.createElement("button");
+  const txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
   span.onclick = closeClick;
   myNodeList[i].appendChild(span);
 
-  var checkbox = myNodeList[i].querySelector('input[type="checkbox"]');
+  const checkbox = myNodeList[i].querySelector('input[type="checkbox"]');
   checkbox.addEventListener('click', function(event) {
-      var parent = event.currentTarget.parentElement;
+      const parent = event.currentTarget.parentElement;
       parent.classList.toggle('checked');
   });
 }
 
 // Create a new list item when clicking on the "Add" button
 function createNewElement() {
-  var inputValue = document.getElementById("myInput").value;
+  const inputValue = document.getElementById("myInput").value;
   if (inputValue === "") {
     alert("You must write something!");
     return;
   }
 
-  var checkbox = document.createElement("input");
+  const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
   checkbox.name = "name";
   checkbox.value = "value";
   checkbox.id = "id";
 
-  var li = document.createElement("li");
-  var textNode = document.createTextNode(inputValue);
+  const li = document.createElement("li");
+  const textNode = document.createTextNode(inputValue);
   li.appendChild(checkbox);
   li.appendChild(textNode);
 
   document.getElementById("myList").appendChild(li);
   document.getElementById("myInput").value = "";
 
-  var span = document.createElement("button");
-  var txt = document.createTextNode("\u00D7");
+  const span = document.createElement("button");
+  const txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
   span.onclick = closeClick;
@@ -47,6 +47,6 @@ function createNewElement() {
 }
 
 function closeClick() {
-  var div = this.parentElement;
+  const div = this.parentElement;
   div.style.display = "none";
 }
